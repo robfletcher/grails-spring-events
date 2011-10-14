adbafdf72997918fcb00ea53463b74a9a8f7f362
Burt Beckwith <burt@burtbeckwith.com>
2011-10-14 01:41:42 -0400

    update to 2.0 for dev

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,17 +2,29 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir	= "target/test-reports"
 grails.project.dependency.resolution = {
-    inherits "global" 
-    log "warn"
-    repositories {        
-        grailsPlugins()
-        grailsHome()
-	mavenCentral()
-    }
-    dependencies {
+	inherits "global"
+	log "warn"
+	repositories {
+		grailsPlugins()
+		grailsHome()
+		mavenLocal()
+		mavenCentral()
+	}
+	dependencies {
 		test("org.hamcrest:hamcrest-all:1.1") {
 			excludes "junit", "easymock", "jmock"
 			exported = false
 		}
-    }
+	}
+
+	plugins {
+
+		build(":release:1.0.0.RC3") {
+			export = false
+		}
+
+		compile(":hibernate:$grailsVersion") {
+			export = false
+		}
+	}
 }
